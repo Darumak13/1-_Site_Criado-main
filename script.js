8ad6435332a2ed9bc240ff74b0a60ed0b980e88f
Daniels <danielvictor65.dv@gmail.com>
2024-05-07 21:47:59 -0300

    Implementação nova tela de inicio

--- script.js
+++ script.js
@@ -5,3 +5,7 @@ function openNav(){
 function closeNav(){
     document.getElementById("myNav").style.width = '0%';
 }
+
+import { Collapse, Ripple, initMDB } from "mdb-ui-kit";
+
+initMDB({ Collapse, Ripple });
